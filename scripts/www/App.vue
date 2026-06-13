deebecc9e76092fc7f36c695ad97d2384d54f0ff
15184898912 <960116565@qq.com>
2026-06-13 21:18:24 +0800

    update src file from pakeplus

---
+++ scripts/www/App.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    onLaunch: function() {
+        console.log('App Launch')
+        // 清除登录状态，确保每次都显示登录页
+        try {
+            uni.removeStorageSync('isLogin')
+            uni.removeStorageSync('userInfo')
+        } catch(e) {
+            console.log(e)
+        }
+    },
+    onShow: function() {
+        console.log('App Show')
+    },
+    onHide: function() {
+        console.log('App Hide')
+    }
+}
+</script>
+
+<style>
+/* 全局样式 */
+page {
+    background-color: #f5f7fb;
+}
+</style>
